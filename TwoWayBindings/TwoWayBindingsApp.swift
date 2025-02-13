bd46852800226e7486933be968b40841725b4476
Benjamin Schmidt <Benny@schmidtb.de>
2025-02-13 12:04:13 +0100

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Benjamin Schmidt on 13.02.25.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
